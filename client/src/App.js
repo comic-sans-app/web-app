dae5d56baf26270e543c90329cd72f6105ad74a0
Katerina Scoullos <katpapaloukas@gmail.com>
2021-01-28 12:35:58 -0500

    (setup): create frontend

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Comic Sans</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
