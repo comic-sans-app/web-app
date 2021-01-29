36860af4743ce1663e7f446ce9a30c3ac381e8e3
Desiree Nelson <palebluedot4@gmail.com>
2021-01-29 14:57:22 -0500

    feature: very basic sidebar component

--- client/src/components/Editor/index.js
+++ client/src/components/Editor/index.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Sidebar } from '../sidebar';
 
 export default function Editor() {
   return (
     <Container className='vh-90' fluid>
       <Row>
-        <Col sm={1} style={{backgroundColor: '#9e9ea3', height: '90vh'}}>
-          {/* SideBar column */}
-          Sidebar
+        <Col sm={1} style={{ height: '90vh' }}>
+          <Sidebar />
         </Col>
 
-        <Col sm={11} style={{backgroundColor: '#e4e4f5'}}>
+        <Col sm={11} style={{ backgroundColor: '#e4e4f5' }}>
           {/* Canvas column */}
           Canvas
         </Col>
       </Row>
     </Container>
-  )
+  );
 }
