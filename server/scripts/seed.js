aed2bb96a062430f07bf4f94243fe193165633eb
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2021-01-30 09:26:09 -0500

    feat: adding a seed file for canvas elements

---
+++ server/scripts/seed.js
@@ -0,0 +1,47 @@
+const db = require('../db')
+const { CanvasElement } = require('../db/models')
+const { canvasElementsData } = require('./data')
+
+// The `seed` function is concerned only with modifying the database.
+async function seed(){
+  await db.sync({force: true})
+  console.log('db synced!')
+
+  const allCanvasElements = await Promise.all(
+    canvasElementsData.map(element =>
+      CanvasElement.create({
+        imageUrl: element.imageUrl
+      })
+    )
+  )
+
+  console.log(`seeded ${allCanvasElements.length} canvas elements`)
+  console.log(`database seeded successfully`)
+}
+
+async function runSeed() {
+  console.log('seeding...')
+  
+  try {
+    await seed()
+  }
+
+  catch (err) {
+    console.error(err)
+    process.exitCode = 1
+  }
+
+  finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
+
+// Execute the `seed` function, IF we ran this module directly (`node seed`).
+if (module === require.main) {
+  runSeed()
+}
+
+// exported for testing purposes
+module.exports = seed
