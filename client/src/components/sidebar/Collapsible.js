a31c3998480e32361cbd7ed60adc2160c211f2bb
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2021-02-02 09:21:22 -0500

    feat: adding a working version of the sidebar, changing z-indexes of all navs and canvas

--- client/src/components/sidebar/Collapsible.js
+++ client/src/components/sidebar/Collapsible.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Button, Container } from 'react-bootstrap'
+import React, { Component } from 'react'
+import '../../styles/Collapsible.css'
 
-export default function Collapsible() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default class Collapsible extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+
+    console.log(this.props)
+
+    return (
+      <div>
+        {/* only render button when sidebar isn't open
+        <Button
+          variant="light"
+          id='collapse-sidebar'
+        >
+          <i className="fas fa-angle-left"></i>
+        </Button>
+
+        {this.state.open ?
+          null
+          :
+          <Container id='collapsible'>
+            SIDE PANEL EXPANDED
+          </Container>
+        } */}
+      </div>
+    )
+  }
 }
