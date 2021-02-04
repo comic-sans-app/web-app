f098b1cba4073fafe04e31a53b2b39e3c47df76b
Desiree Nelson <palebluedot4@gmail.com>
2021-02-04 18:01:37 -0500

    debug: fixed sidebar freeze

--- client/src/components/sidebar/Pages/Templates.js
+++ client/src/components/sidebar/Pages/Templates.js
@@ -7,19 +7,19 @@ import six from '../../../assets/6panel.png';
 const Templates = () => (
   <div className='open-sidebar-panel'>
     <Row>
-      <Col className='sidebar-preview'>
+      <Col className='p-0'>
         {/* When we're ready to, the function to add to canvas will be used with a onClick listener on these images. */}
-        <Image src={three} fluid />3 Panel
+        <Image src={three} className='p-2' fluid />
       </Col>
-      <Col>
-        <Image src={four} fluid />4 Panel
+      <Col className='p-0'>
+        <Image src={four} className='p-2' fluid />
       </Col>
     </Row>
     <Row>
-      <Col>
-        <Image src={six} fluid />6 Panel
+      <Col className='p-0'>
+        <Image src={six} className='p-2' fluid />
       </Col>
-      <Col></Col>
+      <Col className='p-0'></Col>
     </Row>
   </div>
 );
