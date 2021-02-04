6c5a7105ecafcc529094c6ec8d48b7c560f75b49
Desiree Nelson <palebluedot4@gmail.com>
2021-02-04 16:48:12 -0500

    style: side-panel Templates

--- client/src/components/sidebar/Pages/Templates.js
+++ client/src/components/sidebar/Pages/Templates.js
@@ -3,21 +3,22 @@ import { Image, Row, Col } from 'react-bootstrap';
 import three from '../../../assets/3panel.png';
 import four from '../../../assets/4panel.png';
 import six from '../../../assets/6panel.png';
+import {threePanel, fourPanel, sixPanel} from '../../Canvas/Templates'
 
 const Templates = () => (
   <div className='open-sidebar-panel'>
     <Row>
       <Col className='sidebar-preview'>
         {/* When we're ready to, the function to add to canvas will be used with a onClick listener on these images. */}
-        <Image src={three} fluid />3 Panel
+        <Image src={three} fluid rounded />3 Panel
       </Col>
       <Col>
-        <Image src={four} fluid />4 Panel
+        <Image src={four} fluid rounded />4 Panel
       </Col>
     </Row>
     <Row>
       <Col>
-        <Image src={six} fluid />6 Panel
+        <Image src={six} fluid rounded />6 Panel
       </Col>
       <Col></Col>
     </Row>
