d7d896c71ab8fbd4a52e8b77075b294a2402034b
Desiree Nelson <palebluedot4@gmail.com>
2021-02-11 13:37:00 -0500

    added square to act as background when downloading

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -14,7 +14,7 @@ import "../../styles/canvas.css";
 import { fourPanel, threePanel, sixPanel, removePanel } from "./Templates";
 import { AddTextBox } from "./AddTextBox";
 import { Circle } from "../Shapes/Circle";
-import { Square } from "../Shapes/Square";
+import { Square, createBack } from "../Shapes/Square";
 import Bubbles from "../TextBubbles/Bubbles";
 import Characters from "../Characters/characters";
 import { fetchCanvasElements, saveCanvasElements } from "../../store/index";
@@ -86,8 +86,9 @@ class Canvas extends React.Component {
     });
 
   // crossOrigin = anonymous before save needed
-  save = () => {
+  save = (canvasInstance) => {
     var canvas = document.getElementById("canvas");
+    createBack(canvasInstance);
     canvas.toBlob(function (blob) {
       // let downloadedImg = new Image(blob);
       // downloadedImg.crossOrigin = "Anonymous";
@@ -273,7 +274,7 @@ class Canvas extends React.Component {
             placement="top"
             overlay={<Tooltip>{canvasControlsCopy.download}</Tooltip>}
           >
-            <Button variant="light" onClick={() => this.save()}>
+            <Button variant="light" onClick={() => this.save(canvasInstance)}>
               <i className="fas fa-file-download"></i>
             </Button>
           </OverlayTrigger>
@@ -305,7 +306,13 @@ class Canvas extends React.Component {
           </OverlayTrigger>
         </Container>
 
-        <canvas id={`canvas`} width="600" height="600" />
+        <canvas
+          id={`canvas`}
+          width="600"
+          height="600"
+          // backgroundColor='white'
+          // globalAlpha='1'
+        />
       </div>
     );
   }
--- client/src/components/Shapes/Square.js
+++ client/src/components/Shapes/Square.js
@@ -9,3 +9,39 @@ export const Square = (canvas) => {
   canvas.add(square);
   canvas.renderAll();
 };
+
+// creates a white square, the size of the canvas to act as background when saving.
+
+export const createBack = (canvas) => {
+  const backSq = new fabric.Rect({
+    id: "back",
+    height: canvas.height,
+    width: canvas.width,
+    fill: "white",
+    selectable: false,
+  });
+  removeSquare(canvas);
+  canvas.add(backSq);
+  setSquare(canvas);
+  canvas.renderAll();
+};
+
+// Removes previous background
+export const removeSquare = (canvas) => {
+  canvas.getObjects().forEach((obj) => {
+    if (obj.id === "back") {
+      canvas.getActiveObject(obj);
+      canvas.remove(obj);
+    }
+  });
+};
+
+// Sets the background square all the way to bottom layer
+export const setSquare = (canvas) => {
+  canvas.getObjects().forEach((obj) => {
+    if (obj.id === "back") {
+      canvas.getActiveObject(obj);
+      obj.sendToBack();
+    }
+  });
+};
