3ba2ec2c669b13c8ee522b221cbc5334d3d28dd8
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2021-02-12 13:10:27 -0500

    feat: adding images to public folder,  editing seed script to handle accessing images from there

--- server/scripts/seed.js
+++ server/scripts/seed.js
@@ -1,29 +1,90 @@
 const db = require("../db");
 const { CanvasElement, User, Page } = require("../db/models");
 const { canvasElementsData } = require("./data");
+const fs = require("fs");
+const path = require("path");
 
+/*
+    Get all image paths in our sticker folder
+*/
+//created another variable to feed it the file path it wanted
+//then on the next line is where we are construction what is going in the db what it was fed
+//was the file path that relates to the sticker bar component
+//now the string is constructed correctly we don't need to change file because the function cycles through that colder
+
+//path.join('..', '..', 'images', 'stickers')
+
+const createCharactersData = function (
+  charactersDir = path.join(
+    __dirname,
+    "../../",
+    "client/public/images/characters"
+  ),
+  charactersData = []
+) {
+  fs.readdirSync(charactersDir).forEach((file) => {
+    // file is a name like blue.PNG etc
+    const path = `${charactersDir}/${file}`;
+
+    if (
+      path.endsWith(".jpg") ||
+      path.endsWith(".png") ||
+      path.endsWith(".PNG")
+    ) {
+      charactersData.push({
+        imageUrl: `${path}`,
+        type: "character",
+      });
+    }
+  });
+
+  return charactersData;
+};
+
+const createSpeechBubblesData = function (
+  bubblesDir = path.join(__dirname, "../../", "client/public/images/bubbles"),
+  speechBubblesData = []
+) {
+  fs.readdirSync(bubblesDir).forEach((file) => {
+    // file is a name like blue.PNG etc
+    const path = `${bubblesDir}/${file}`;
+
+    if (
+      path.endsWith(".jpg") ||
+      path.endsWith(".png") ||
+      path.endsWith(".PNG")
+    ) {
+      speechBubblesData.push({
+        imageUrl: `${path}`,
+        type: "bubble",
+      });
+    }
+  });
+
+  return speechBubblesData;
+};
+
+const charactersSeedData = createCharactersData();
+const bubblesSeedData = createSpeechBubblesData();
 
 // The `seed` function is concerned only with modifying the database.
 async function seed() {
   await db.sync({ force: true });
-  
+
   console.log("db synced!");
 
-  const allCanvasElements = await Promise.all(
-    canvasElementsData.map((element) =>
-      CanvasElement.create({
-        imageUrl: element.imageUrl,
-        type: element.type,
-      })
-    )
+  await CanvasElement.bulkCreate(charactersSeedData, { validate: true });
+  console.log(
+    `seeded ${charactersSeedData.length} canvas elements for characters`
   );
 
-  console.log(`seeded ${allCanvasElements.length} canvas elements`);
+  await CanvasElement.bulkCreate(bubblesSeedData, { validate: true });
+  console.log(`seeded ${bubblesSeedData.length} canvas elements for bubbles`);
+
   console.log(`database seeded successfully`);
 }
 
 async function runSeed() {
-  
   console.log("seeding...");
 
   try {
