1d6b6005db75d7cefbdc46494e50480ddfdd2d6e
Laura Kathryn Gilbert <laura.kate.gilbert@gmail.com>
2021-02-13 18:50:52 -0500

    taking out unnecessary line and changing one word, haha

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -20,7 +20,6 @@ import Characters from "../Characters/characters";
 import { fetchCanvasElements, saveCanvasElements } from "../../store/index";
 import ColorPicker from "../Editor/ColorPicker";
 import { canvasControlsCopy } from "./Copy";
-import BrushWidthSlider from "./BrushWidthSlider";
 
 let windowHeightRatio = Math.floor(0.7 * window.innerHeight);
 let windowWidthRatio = Math.floor(0.7 * window.innerWidth);
--- client/src/components/Canvas/Copy.js
+++ client/src/components/Canvas/Copy.js
@@ -1,10 +1,10 @@
 export const canvasControlsCopy = {
-  bringUp: "bring element on the uppermost layer",
-  bringDown: "bring element on the lowest layer",
+  bringUp: "bring element to the uppermost layer",
+  bringDown: "bring element to the lowest layer",
   save: "save changes",
   download: "download as png",
   delete: "delete selected elements",
-  bringUpOne: 'bring element forward one layer',
-  bringDownOne: 'bring element down one layer',
-  clearCanvas: 'remove all elements from canvas',
+  bringUpOne: "bring element forward one layer",
+  bringDownOne: "bring element down one layer",
+  clearCanvas: "remove all elements from canvas",
 };
